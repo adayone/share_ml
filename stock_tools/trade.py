07b367480b1be0d9859936c1c971b1196892d91d
haoyuan.huhy <a7758526@gmail.com>
2015-06-25 23:47:18 +0800

    new

--- stock_tools/trade.py
+++ stock_tools/trade.py
@@ -3,6 +3,10 @@ import tushare as ts
 def get_new_stock():
     news = ts.get_gem_classified()
     id = news['code']
-    return id.apply(lambda x: ts.get_h_data(x, start='2013-01-01', end='2015-06-20'))
+    return id.apply(lambda x: get_data(x))
 
 
+def get_data(id):
+    df = ts.get_hist_data(x, start='2013-01-01', end='2015-06-20')
+    df['code'] = id
+    return df
